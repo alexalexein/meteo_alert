42aee32974b3d4f993eaa3a43fdf585cf42627bb
Alex Garcia <alexalexein@hotmail.com>
2018-04-23 11:25:00 +0200

    daily forecast working

--- Padir/app/src/main/java/agarcia/padir/dailyForecastFragment.java
+++ Padir/app/src/main/java/agarcia/padir/dailyForecastFragment.java
@@ -16,6 +16,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.List;
 
 import static agarcia.padir.R.id.alarmRecyclerView;
@@ -32,6 +33,22 @@ public class dailyForecastFragment extends Fragment {
     private TextView debugTextView;
     private RecyclerView recyclerView;
     private dailyAdapter recyclerViewAdapter;
+
+    // SKY STATES PRECIPITATION
+    private final int NO_PRECIPITATION = 1;
+    private final int RAIN = 2;
+    private final int SNOW = 3;
+    private final int SCARCE_RAIN = 4;
+    private final int SCARCE_SNOW = 7;
+
+    // SKY STATE CLOUDS
+    private final int CLEAR = 1;
+    private final int SLIGTHLY_CLOUDY = 2;
+    private final int PARTLY_CLOUDY = 3;
+    private final int MOSTLY_CLOUDY = 4;
+    private final int CLOUDY = 5;
+    private final int OVERCAST_SKY = 6;
+    private final int HIGH_CLOUDS = 7;
 
     // newInstance constructor for creating fragment with arguments
     public static dailyForecastFragment newInstance(int page, String title) {
@@ -135,11 +152,11 @@ public class dailyForecastFragment extends Fragment {
         @Override
         public void onBindViewHolder(final dailyForecastFragment.dailyHolder holder, final int position) {
             holder.dateTextView.setText(getDateText(position));
-            holder.skyStateImageView.setImageResource(getIcon(Integer.valueOf(skyStateString[position])));
+            holder.skyStateImageView.setImageResource(getIcon(skyStateString[position]));
             holder.oddsTextView.setText(oddsString[position] + " %");
             holder.maxTempTextView.setText(maxTempString[position]);
             holder.minTempTextView.setText(minTempString[position]);
-            holder.windDirTextView.setText(windDirString[position]);
+            holder.windDirTextView.setText(getWindDir(position));
             holder.windSpeedTextView.setText(windSpeedString[position] + " km/h");
         }
 
@@ -148,12 +165,134 @@ public class dailyForecastFragment extends Fragment {
             return 6;
         }
 
-        private int getIcon(int skyStateCode){
-            return R.mipmap.clear_icon;
+        private String getWindDir(int position){
+            String result;
+            switch (windDirString[position]){
+                case "S":
+                    result = getResources().getString(R.string.southAbbr);
+                    break;
+                case "E":
+                    result = getResources().getString(R.string.eastAbbr);
+                    break;
+                case "O":
+                    result = getResources().getString(R.string.westAbbr);
+                    break;
+                case "N":
+                    result = getResources().getString(R.string.northAbbr);
+                    break;
+                case "SE":
+                    result = getResources().getString(R.string.southEastAbbr);
+                    break;
+                case "SO":
+                    result = getResources().getString(R.string.southWestAbbr);
+                    break;
+                case "NE":
+                    result = getResources().getString(R.string.northEastAbbr);
+                    break;
+                case "NO":
+                    result = getResources().getString(R.string.northWestAbbr);
+                    break;
+                default:
+                    result = "unknown";
+            }
+            return result;
+        }
+
+        private int getIcon(String skyStateCode){
+            String skyStateCode_0;
+            String skyStateCode_1;
+            int icon;
+            if (skyStateCode != "" && !skyStateCode.equals("") && skyStateCode != null){
+                skyStateCode_0 = Character.toString(skyStateCode.charAt(0));
+                skyStateCode_1 = Character.toString(skyStateCode.charAt(1));
+            }
+            else {
+                skyStateCode_0 = "0";
+                skyStateCode_1 = "0";
+            }
+            switch (Integer.valueOf(skyStateCode_0)){
+                case NO_PRECIPITATION:
+                    switch (Integer.valueOf(skyStateCode_1)){
+                        case CLEAR:
+                            icon = R.mipmap.clear_icon;
+                            break;
+                        case SLIGTHLY_CLOUDY:
+                            icon = R.mipmap.slightly_partly_cloudy_icon;
+                            break;
+                        case PARTLY_CLOUDY:
+                            icon = R.mipmap.slightly_partly_cloudy_icon;
+                            break;
+                        case MOSTLY_CLOUDY:
+                            icon = R.mipmap.mostly_overcast_cloudy_icon;
+                            break;
+                        case CLOUDY:
+                            icon = R.mipmap.mostly_overcast_cloudy_icon;
+                            break;
+                        case OVERCAST_SKY:
+                            icon = R.mipmap.mostly_overcast_cloudy_icon;
+                            break;
+                        case HIGH_CLOUDS:
+                            icon = R.mipmap.high_clouds_icon;
+                            break;
+                        default:
+                            icon = R.mipmap.weather_icon;
+                            break;
+                    }
+                    break;
+                case RAIN:
+                    icon = R.mipmap.rain_icon;
+                    break;
+                case SNOW:
+                    icon = R.mipmap.snow_icon;
+                    break;
+                case SCARCE_RAIN:
+                    icon = R.mipmap.scarce_rain_icon;
+                    break;
+                case SCARCE_SNOW:
+                    icon = R.mipmap.scarce_snow_icon;
+                    break;
+                default:
+                    icon = R.mipmap.weather_icon;
+                    break;
+
+            }
+            return icon;
         }
 
         private String getDateText(int position){
-            return "Monday 12/04";
+            Calendar currentCalendarInstance = Calendar.getInstance();
+            currentCalendarInstance.add(Calendar.DAY_OF_YEAR, position + 1);
+            String result;
+            switch (currentCalendarInstance.get(Calendar.DAY_OF_WEEK)){
+                case 1:
+                    result = getResources().getString(R.string.sundayName);
+                    break;
+                case 2:
+                    result= getResources().getString(R.string.mondayName);
+                    break;
+                case 3:
+                    result = getResources().getString(R.string.tuesdayName);
+                    break;
+                case 4:
+                    result = getResources().getString(R.string.wednesdayName);
+                    break;
+                case 5:
+                    result = getResources().getString(R.string.thursdayName);
+                    break;
+                case 6:
+                    result = getResources().getString(R.string.fridayName);
+                    break;
+                case 7:
+                    result = getResources().getString(R.string.saturdayName);
+                    break;
+                default:
+                    result = "unknown";
+                    break;
+            }
+            result = result + ", " +
+                    String.valueOf(currentCalendarInstance.get(Calendar.DAY_OF_MONTH)) + "/" +
+                    String.valueOf(currentCalendarInstance.get(Calendar.MONTH));
+            return result;
         }
     }
 }
